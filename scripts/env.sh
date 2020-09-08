6eecdeb47bd5be44b3d59b7a55a5f6a3bff669ac
Sergei Voronezhskii <70293896+g9d@users.noreply.github.com>
2020-09-08 14:35:24 +0300

    Fix q-server to stable version

--- scripts/env.sh
+++ scripts/env.sh
@@ -20,6 +20,6 @@ export EMAIL_FOR_NOTIFICATIONS="email@yourdomain.com"
 export COMPOSE_HTTP_TIMEOUT=120 # in sec, 60 sec - default
 HOSTNAME=$(hostname -f)
 export TON_Q_SERVER_GITHUB_REPO="https://github.com/tonlabs/ton-q-server"
-export TON_Q_SERVER_GITHUB_COMMIT_ID="master"
+export TON_Q_SERVER_GITHUB_COMMIT_ID="0.27.9"
 export TON_NODE_GITHUB_REPO="https://github.com/tonlabs/ton-labs-node"
 export TON_NODE_GITHUB_COMMIT_ID="master"
