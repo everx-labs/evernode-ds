94a5fa5d5ce6d3bf13514266a829ffe8f8708589
Artem Ryabov <artem.r@tonlabs.io>
2021-07-20 02:59:29 +0300

    Added node compression support

--- docker-compose/ton-node/build/entrypoint.sh
+++ docker-compose/ton-node/build/entrypoint.sh
@@ -6,7 +6,16 @@ echo "INFO: NETWORK_TYPE = ${NETWORK_TYPE}"
 echo "INFO: CONFIGS_PATH = ${CONFIGS_PATH}"
 echo "INFO: \$1 = $1"
 
-curl -sS "https://raw.githubusercontent.com/tonlabs/${NETWORK_TYPE}/master/configs/${NETWORK_TYPE}/ton-global.config.json" \
+case "${NETWORK_TYPE}" in
+"net.ton.dev" | "main.ton.dev")
+    TON_GLOBAL_CONFIG_BRANCH="master"
+    ;;
+"rustnet.ton.dev")
+    TON_GLOBAL_CONFIG_BRANCH="main"
+    ;;
+esac
+
+curl -sS "https://raw.githubusercontent.com/tonlabs/${NETWORK_TYPE}/${TON_GLOBAL_CONFIG_BRANCH}/configs/${NETWORK_TYPE}/ton-global.config.json" \
     -o "${CONFIGS_PATH}/ton-global.config.json"
 
 if [ "$1" = "bash" ]; then
