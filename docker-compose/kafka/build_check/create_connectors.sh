d79f7aac18c84da21994d2c7367637527c1e300b
aferno <p.rasputin@everx.dev>
2023-04-12 15:49:41 +0300

    Update scripts and versions

--- docker-compose/kafka/build_check/create_connectors.sh
+++ docker-compose/kafka/build_check/create_connectors.sh
@@ -4,6 +4,6 @@ set -e
 
 for connector in $(ls /work/connectors)
 do
-    connect-cli create -e http://connect:8083/  $(awk -F "." '{print $1}' <<< ${connector}) < /work/connectors/${connector}
+    java -jar /usr/bin/connect-cli create -e http://connect:8083/  $(awk -F "." '{print $1}' <<< ${connector}) < /work/connectors/${connector}
     sleep 10s
 done
